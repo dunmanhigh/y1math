35346886c5541bfe0e5c55ed933a638ac8d1c266
Peanutella-Hub <53025282+Peanutella-Hub@users.noreply.github.com>
2019-10-17 13:58:34 +0800

    Update t07numberpatterns_multiples3.py

--- t07numberpatterns_multiples3.py
+++ t07numberpatterns_multiples3.py
@@ -1,8 +1,8 @@
-# Generate multiples of 3 from 1 to 50
+# Generate multiples of 5 from 1 to 100
 
 START = 1
-END = 50
-num = 3
+END = 100
+num = 5
 
 for i in range(START, END+1):
   if i % num == 0:
