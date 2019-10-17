3d3995c4217b8ea564a5a24123cdd3850b6af615
SchwarzMann07 <53025404+SchwarzMann07@users.noreply.github.com>
2019-10-17 13:56:55 +0800

    factor of 12

--- t01primeshcflcm_factors1.py
+++ t01primeshcflcm_factors1.py
@@ -1,8 +1,8 @@
-# What are the factors of 18?
-# factor: an integer which when multiplied with another integer, results in the product 18. 
+# What are the factors of 12?
+# factor: an integer which when multiplied with another integer, results in the product 12. 
 # Hence when 18 is divided by this number, the dividend is an integer and there are no remainders.
 
-num = 18
+num = 12
 
 for i in range(1, num+1):  
   if num % i == 0:
