80d920608f293e95aaab677e4ca6a4e8f64711f1
AmandaTohEnmin <52941539+AmandaTohEnmin@users.noreply.github.com>
2019-10-17 14:16:46 +0800

    New Formula
    Dear editor, 
    I have made changes.

--- t01primeshcflcm_evennumbers.py
+++ t01primeshcflcm_evennumbers.py
@@ -1,6 +1,6 @@
 # What are the even numbers from 1 to num?
 
 num = 123
-for i in range(1, num+1):
-  if i % 2 == 0:
+for i in range(1, num+1): # 1 to 124
+  if i % 2 == 0:          # i = 1
     print(i, end=' ')
