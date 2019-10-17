e738b92cd30b3e3d7ca7066793e89500561dbabd
liangni9135 <52898000+liangni9135@users.noreply.github.com>
2019-10-17 13:56:17 +0800

    Update t01primeshcflcm_hcf1.py

--- t01primeshcflcm_hcf1.py
+++ t01primeshcflcm_hcf1.py
@@ -1,2 +1,11 @@
 # Find the Highest Common Factor (HCF) of 18 and 30.
+num = 18
 
+for i in range(1, num+1):  
+  if num % i == 0:
+    print(i, end=' ')
+num = 30
+
+for i in range(1, num+1):  
+  if num % i == 0:
+    print(i, end=' ')
