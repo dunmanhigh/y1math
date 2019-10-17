fdd90123223e3a51baf0bf935412603deef9c5b3
MinJiaTse <48120696+MinJiaTse@users.noreply.github.com>
2019-10-17 14:26:43 +0800

    Update t01primeshcflcm_findab2.py

--- t01primeshcflcm_findab2.py
+++ t01primeshcflcm_findab2.py
@@ -1,2 +1,2 @@
 # If a and b are integers such that a * b = 18, find the possible values of a + b.
-
+print("a can be 1, and b is 18. a can be 2 and b is 9. a can be 3 and b is 6. and vice-versa. So there is a total of 6 possible ways")
