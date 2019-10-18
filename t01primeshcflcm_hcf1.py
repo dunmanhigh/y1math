423fbe174460ffa99961e25a034df3bc8a74b0bb
dhsleechenyidamian <38306877+dhsleechenyidamian@users.noreply.github.com>
2019-10-18 14:14:43 +0800

    hcf bro
    _please_

--- t01primeshcflcm_hcf1.py
+++ t01primeshcflcm_hcf1.py
@@ -1,2 +1,11 @@
 # Find the Highest Common Factor (HCF) of 18 and 30.
-
+num1 = int(input("Enter first number: "))         
+    
+num2 = int(input("Enter second number: "))              
+hcf = 1
+for i in range(min(num1,num2),0,-1):                    
+    if (num1%i) == 0 and (num2%i) == 0:
+        hcf = i
+        break
+    
+print(hcf)
