414b0462ee7adb87ef0efa8752b14fd81fa120a1
CaydenTan-310 <53048966+CaydenTan-310@users.noreply.github.com>
2019-10-18 16:47:28 +0800

    Update t01primeshcflcm_factors2.py

--- t01primeshcflcm_factors2.py
+++ t01primeshcflcm_factors2.py
@@ -1,3 +1,22 @@
 # Generate a list containing factors of num.
 
-num = 
+multiplication = 18
+
+factors = []
+factors2 = []
+e=0
+for i in range(1, multiplication+1):
+    if multiplication % i == 0:
+        e+=1
+e=e/2
+for i in range(1, multiplication+1):
+    if multiplication % i == 0:
+        if (i <= e):
+            factors.append(i)
+        else:
+            factors2.append(i)
+
+e=0
+while e!=len(factors):
+    print(factors[e]+factors2[0-(e+1)])
+    e+=1
