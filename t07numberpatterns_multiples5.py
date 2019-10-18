0f17e2f04c3f8e3c07d68af505ebb26a0d5e7e2e
mingiandhs <48120781+mingiandhs@users.noreply.github.com>
2019-10-18 15:37:37 +0800

    Update t07numberpatterns_multiples5.py

--- t07numberpatterns_multiples5.py
+++ t07numberpatterns_multiples5.py
@@ -1,9 +1,7 @@
 # Generate multiples of 5 from 1 to 100
 
-START = 1
-END = 100
-num = 
-
-for i in range(START, ):
-  if i % num == 0:
-    print(i, end=' ')
+x = 1 
+while x < 20:
+  num = 0 
+  num = num + 5 
+  print (num)
