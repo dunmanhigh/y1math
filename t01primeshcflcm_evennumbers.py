4160fdd7d339822cfd5bba44315f0f6d44015542
dhstanshientrishaann <37992168+dhstanshientrishaann@users.noreply.github.com>
2019-10-18 14:47:53 +0800

    Update t01primeshcflcm_evennumbers.py

--- t01primeshcflcm_evennumbers.py
+++ t01primeshcflcm_evennumbers.py
@@ -1,6 +1,10 @@
 # What are the even numbers from 1 to num?
-
+# num is 123
 num = 123
+
+# execute a loop from 1 to 123
 for i in range(1, num+1):
+  # check if the current number is divisible by 2 
   if i % 2 == 0:
+    #print out the number, in the same line
     print(i, end=' ')
