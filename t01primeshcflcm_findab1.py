0e24b78565413d70d9d55c24a2d830081311ac34
sawzedong <48119998+sawzedong@users.noreply.github.com>
2019-10-18 15:34:03 +0800

    Update t01primeshcflcm_findab1.py

--- t01primeshcflcm_findab1.py
+++ t01primeshcflcm_findab1.py
@@ -1,2 +1,12 @@
 # If a and b are integers such that a * b = 17, find the value of a + b.
 
+num = 17
+factors = []
+
+for i in range(1, num+1):  
+  if num % i == 0:
+    factors.append(i)
+    
+aplusb = factors[0] + factors[1]
+print("a + b = " + str(aplusb))
+
