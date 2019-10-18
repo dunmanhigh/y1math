33f83d930500e845817639a1c688db453029d8d3
MinJiaTse <48120696+MinJiaTse@users.noreply.github.com>
2019-10-18 15:42:40 +0800

    Update t01primeshcflcm_primemore.py

--- t01primeshcflcm_primemore.py
+++ t01primeshcflcm_primemore.py
@@ -1,3 +1,14 @@
 # What is the prime number greater than num?
 
 num = int(input("num: "))
+theNum = num
+something = True
+while something:
+  theNum += 1
+  if (theNum % 1 == theNum) and (theNum % theNum == 1):
+    for i in range(2, theNum):
+      if theNum % i == 0:
+        theNum += 1
+      else:
+        something = False
+print("the prime number greater than num is \(theNum)")
