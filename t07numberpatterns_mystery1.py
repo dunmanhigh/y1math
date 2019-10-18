216382522dafbb488be4c4786f6af86822b93cc9
dhsbensze <38306856+dhsbensze@users.noreply.github.com>
2019-10-18 14:21:25 +0800

    Attempted.

--- t07numberpatterns_mystery1.py
+++ t07numberpatterns_mystery1.py
@@ -2,4 +2,36 @@
 
 message = 'The quick yellow fox jumps over the lazy dog.'
 
-cipher = ''
+cipher = 'shift(-1)'
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j','k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def shift(offset):
+
+    message = input("The quick yellow fox jumps over the lazy dog:\n")
+    new_message = ''
+
+    for letter in message:
+
+        letter = letter.lower() #doesn't handle upper-case yet
+
+        if letter.isalpha():
+            shift_pos = alphabet.index(letter) + offset
+            new_pos = alphabet[shift_pos]
+            new_message += new_pos
+
+        #these will not be shifted
+
+        elif ' ' or '/t' or '/n' in letter: 
+            new_message += letter
+
+        elif letter.isnumeric(): 
+            new_message += letter
+
+        else:
+            print("An error took place in recording the message. Check input.\n")
+
+    print(new_message)
+
+
+shift(-1)
