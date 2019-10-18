5da3cd97dccd30d8eed30d6d46e64544738f74f9
dhschuachloe <38306799+dhschuachloe@users.noreply.github.com>
2019-10-18 14:18:42 +0800

    Update t01primeshcflcm_factors1.py

--- t01primeshcflcm_factors1.py
+++ t01primeshcflcm_factors1.py
@@ -7,3 +7,21 @@ num = 18
 for i in range(1, num+1):  
   if num % i == 0:
     print(i, end=' ')
+# Python Program to find the factors of a number
+
+# define a function
+def print_factors(x):
+   # This function takes a number and prints the factors
+
+   print("The factors of",x,"are:")
+   for i in range(1, x + 1):
+       if x % i == 0:
+           print(i)
+
+# change this value for a different result.
+num = 320
+
+# uncomment the following line to take input from the user
+#num = int(input("Enter a number: "))
+
+print_factors(num)
