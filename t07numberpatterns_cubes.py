ee444d44821f804cf06f591edc7dd427ea517e57
dhslimzhiyan <38306798+dhslimzhiyan@users.noreply.github.com>
2019-10-18 14:21:02 +0800

    add cubes of numbers from 1 to 10

--- t07numberpatterns_cubes.py
+++ t07numberpatterns_cubes.py
@@ -1,3 +1,13 @@
 # Generate cubes number pattern from 1 to 10
 # 1 8 27 64 ...
+for number in range(1, 10):
+    base = number * (number -1) + 1
 
+    output = str.format("{}^3 = {}", number, base)
+    base += 2
+    
+    for i in range(0, number - 1):
+        output += str.format(" + {}", base)
+        base += 2
+        
+    print(output)
