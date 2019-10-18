467e1a537a9654133080ba4a855491a3a07a1c90
dhsgarethlimkaiyang <38306969+dhsgarethlimkaiyang@users.noreply.github.com>
2019-10-18 14:24:04 +0800

    update code

--- t07numberpatterns_oscillating1.py
+++ t07numberpatterns_oscillating1.py
@@ -1,6 +1,18 @@
 # Generate oscillating pattern 1, -1, 1, -1, 1, -1, ... k times
 
-k = 10
-
-for i in range(k):
-  
+print('What is k?')
+k = int(input())
+print('How many times do you want to repeat the loop?')
+counter = int(input())
+x=1
+while counter > 0:
+    if x ==1:
+        k = k + 1
+        x = x + 1
+        counter = int(counter - 1)
+        print(k)
+    if x == 2:
+        k = k - 1
+        x = x - 1
+        counter = int(counter - 1)
+        print(k)
