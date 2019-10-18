d89300b01068538a9203675fa8872f0ec746d133
dhslimjeff <38306926+dhslimjeff@users.noreply.github.com>
2019-10-18 14:12:12 +0800

    solution of lcm 18, 30 yes

--- t01primeshcflcm_lcm1.py
+++ t01primeshcflcm_lcm1.py
@@ -1,2 +1,23 @@
 # Find the Lowest Common Multiple (LCM) of 18 and 30.
+def lcm(x, y):
+   
 
+   if x > y:
+       greater = x
+   else:
+       greater = y
+
+   while(True):
+       if((greater % x == 0) and (greater % y == 0)):
+           lcm = greater
+           break
+       greater += 1
+
+   return lcm
+
+
+num1 = 18
+num2 = 30
+
+
+print("The L.C.M. of", num1,"and", num2,"is", lcm(num1, num2))
