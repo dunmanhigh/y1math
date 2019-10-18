f0be2fa05abe33b926c5dd9ecb8286b20f061ce9
sawzedong <48119998+sawzedong@users.noreply.github.com>
2019-10-18 16:59:26 +0800

    Update t07numberpatterns_oscillating1.py

--- t07numberpatterns_oscillating1.py
+++ t07numberpatterns_oscillating1.py
@@ -1,6 +1,22 @@
 # Generate oscillating pattern 1, -1, 1, -1, 1, -1, ... k times
 
-k = 10
+# Generate oscillating pattern 1, -1, 1, -1, 1, -1, ... k times
 
-for i in range(k):
-  
+k = int(input()) #read input for k
+even = (k%2 == 0) #check if k is even
+pattern = []
+
+if even:
+    times = int(k/2)
+    for i in range(times):
+        pattern.append(1)
+        pattern.append(-1)
+    print(pattern)
+
+else:
+    times = int((k-1)/2)
+    for i in range(times):
+        pattern.append(1)
+        pattern.append(-1)
+    pattern.append(1)
+    print(pattern)
