3a5bba92c6ccfdf99ff091302e1d4a6772d14150
dhsyapjiaqian <38306746+dhsyapjiaqian@users.noreply.github.com>
2019-10-18 14:14:09 +0800

    add answer

--- t07numberpatterns_squares.py
+++ t07numberpatterns_squares.py
@@ -1,3 +1,15 @@
 # Generate squares number pattern from 1 to 10
 # 1 4 9 16 25 ...
 
+<body>
+  <li>Square of 1:1
+  <li>Square of 2:4
+  <li>Square of 3:9
+  <li>Square of 4:16
+  <li>Square of 5:25
+  <li>Square of 6:36
+  <li>Square of 7:49
+  <li>Square of 8:64
+  <li>Square of 9:81
+  <li>Square of 10:100
+</body>
