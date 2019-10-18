83e991b1a461abff44ca4a2e5d0b74f88bc0d7d4
dhslauenxingrace <38306814+dhslauenxingrace@users.noreply.github.com>
2019-10-18 14:19:44 +0800

    Grace Lau En Xin 2J
    Please consider the following changes

--- t01primeshcflcm_lcm1.py
+++ t01primeshcflcm_lcm1.py
@@ -1,2 +1,18 @@
 # Find the Lowest Common Multiple (LCM) of 18 and 30.
 
+def lcm(30, 18):  
+   if 30 > 18:  
+       greater = 30  
+   else:  
+       greater = 18  
+  while(True):  
+       if((greater % x == 0) and (greater % y == 0)):  
+           lcm = greater  
+           break  
+       greater += 1  
+   return lcm  
+  
+  
+num1 = int(input("Enter first number: "))  
+num2 = int(input("Enter second number: "))  
+print("The L.C.M. of", num1,"and", num2,"is", lcm(num1, num2))  
