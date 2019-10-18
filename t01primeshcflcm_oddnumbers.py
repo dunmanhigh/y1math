6cbb1c6871406b80c12f8eac196145d859a4550c
dhschuachloe <38306799+dhschuachloe@users.noreply.github.com>
2019-10-18 14:20:20 +0800

    Update t01primeshcflcm_oddnumbers.py

--- t01primeshcflcm_oddnumbers.py
+++ t01primeshcflcm_oddnumbers.py
@@ -4,3 +4,13 @@ num = 123
 for i in range(1, num+1):
   if i % 2 == 1: # or != 0
     print(i, end=' ')
+# Python program to print odd Numbers in given range 
+  
+start, end = 4, 19
+  
+# iterating each number in list 
+for num in range(start, end + 1): 
+      
+    # checking condition 
+    if num % 2 != 0: 
+        print(num, end = " ") 
