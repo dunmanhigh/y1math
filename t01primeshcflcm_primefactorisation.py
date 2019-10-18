58079b94b09a8495174b54c0ab8b8a2c6efd1ddf
dhswoonzeening <38306895+dhswoonzeening@users.noreply.github.com>
2019-10-18 14:20:40 +0800

    add 2, 3^2

--- t01primeshcflcm_primefactorisation.py
+++ t01primeshcflcm_primefactorisation.py
@@ -1,3 +1,6 @@
 # The process of expressing a number as a product of its prime factors is called prime factorisation.
 # Express 18 as a prime factorisation.
 
+primeFactors(int 18); 
+return 2,3^2
+print(2,3^2)
