8629ad31e7604a0855c290b269e1cdfa22a3d97d
dhslimjeff <38306926+dhslimjeff@users.noreply.github.com>
2019-10-18 14:17:42 +0800

    solution?
    yes

--- t01primeshcflcm_lcm3.py
+++ t01primeshcflcm_lcm3.py
@@ -1,1 +1,19 @@
 # Find the LCM of three numbers
+from math import gcd
+
+#3 digit lcm calculation
+h=input("(1) 2 Digit LCM Or \n(2) 3 Digit LCM\n :")
+if h == "2":
+    while True:
+        def lcm(x, y, z):
+            gcd2 = gcd(y, z)
+            gcd3 = gcd(x, gcd2)
+
+            lcm2 = y*z // gcd2
+            lcm3 = x*lcm2 // gcd(x, lcm2)
+            return lcm3
+
+        x = int(input("Number 1: "))
+        y = int(input("Number 2: "))
+        z = int(input("Number 3: "))
+        print("The LCM Of " + str(x) + " And " + str(y) + " And " + str(z) + " Is " + str(lcm(x, y, z)))
