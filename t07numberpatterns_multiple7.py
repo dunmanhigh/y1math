f262e7e80dc092616a05b76a007b8bf38f28880f
dhslauenxingrace <38306814+dhslauenxingrace@users.noreply.github.com>
2019-10-18 14:29:49 +0800

    added answer

--- t07numberpatterns_multiple7.py
+++ t07numberpatterns_multiple7.py
@@ -1,5 +1,8 @@
 # Generate multiples of 7 from 1 to 100
 
-def printMultiples(7, 100):
-for 7 in (7,100):
-    print(7, end = ' ')
+START = 1 
+END = 100
+num = 7
+
+for i in range(START, END+1):
+    print(i * num)
