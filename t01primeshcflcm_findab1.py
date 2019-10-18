c8888454938b64a25c701b73c8da909cf9833b1f
dhscheechengyinnsheri <38306761+dhscheechengyinnsheri@users.noreply.github.com>
2019-10-18 14:10:38 +0800

    Update t01primeshcflcm_findab1.py

--- t01primeshcflcm_findab1.py
+++ t01primeshcflcm_findab1.py
@@ -1,2 +1,7 @@
 # If a and b are integers such that a * b = 17, find the value of a + b.
 
+#Solution:
+#17 is a prime number, so
+17 = 1 * 17
+
+#Hence, the only numbers of a and b can be 1 and 17, so a + b = 18
