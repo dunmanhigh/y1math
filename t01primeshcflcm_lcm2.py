e0bd3385f82364ce67317aa977ae8c6a381d482d
dhsyukaixigabrielle <38306837+dhsyukaixigabrielle@users.noreply.github.com>
2019-10-18 14:15:16 +0800

    solution added

--- t01primeshcflcm_lcm2.py
+++ t01primeshcflcm_lcm2.py
@@ -1,6 +1,29 @@
 # To find LCM, we can use prime factorisation.
-# Step 1: Obtain the prime factorisation of each number
-# Step 2: Identify the common prime factors
-# Step 3: The LCM of the two numbers is the product of the common prime factors 
-#         and all the other prime factors
 
+def lcm(x, y):
+   """This function takes two
+   integers and returns the L.C.M."""
+
+   # choose the greater number
+   if x > y:
+       greater = x
+   else:
+       greater = y
+
+   while(True):
+       if((greater % x == 0) and (greater % y == 0)):
+           lcm = greater
+           break
+       greater += 1
+
+   return lcm
+
+# change the values of num1 and num2 for a different result
+num1 = 54
+num2 = 24
+
+# uncomment the following lines to take input from the user
+#num1 = int(input("Enter first number: "))
+#num2 = int(input("Enter second number: "))
+
+print("The L.C.M. of", num1,"and", num2,"is", lcm(num1, num2))
