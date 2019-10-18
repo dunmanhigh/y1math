c01cfe4ac96cd405a8fb7a4641bbe12119b1df8a
dhsyukaixigabrielle <38306837+dhsyukaixigabrielle@users.noreply.github.com>
2019-10-18 14:04:20 +0800

    added solution

--- t07numberpatterns_powers3.py
+++ t07numberpatterns_powers3.py
@@ -1,8 +1,7 @@
 # Generate powers of 3 from 1 to 13
 
-START = 
-END = 
-num = 
-
-for i in range(START, END+1):
-    print(, end=' ')
+num = 1
+where num < 14:
+    cube = num * num * num
+    print(cube)
+    num = num + 1
