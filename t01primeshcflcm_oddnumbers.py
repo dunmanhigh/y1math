7e1003dc9b4e1a189661bec368b9ea449aa82556
MythiCatz <53025317+MythiCatz@users.noreply.github.com>
2019-10-19 21:07:55 +0800

    Update t01primeshcflcm_oddnumbers.py

--- t01primeshcflcm_oddnumbers.py
+++ t01primeshcflcm_oddnumbers.py
@@ -1,6 +1,7 @@
 # What are the odd numbers from 1 to num?
 
+print('Odd numbers from 1 to 123')
 num = 123
 for i in range(1, num+1):
-  if i % 2 == 1: # or != 0
+  if i % 2 in range(1, num):
     print(i, end=' ')
