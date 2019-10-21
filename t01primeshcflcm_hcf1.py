ccaf5b59607aae3b5922e59eed34ae08ae9ce908
dhsjanelletan <38057030+dhsjanelletan@users.noreply.github.com>
2019-10-21 14:00:18 +0800

    Update answers and questions

--- t01primeshcflcm_hcf1.py
+++ t01primeshcflcm_hcf1.py
@@ -1,2 +1,4 @@
-# Find the Highest Common Factor (HCF) of 18 and 30.
+# Find the Lowest Common Multiple of 20 and 5
+# Lowest Common Multiple is the smallest number that can be divided by both numbers.
 
+Ans = 40 
