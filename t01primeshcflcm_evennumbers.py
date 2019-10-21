22ae39bedc422f50ef68163956a85ec5e5de596c
dhsngochongxi <38057056+dhsngochongxi@users.noreply.github.com>
2019-10-21 14:02:34 +0800

    added question if num is odd or even

--- t01primeshcflcm_evennumbers.py
+++ t01primeshcflcm_evennumbers.py
@@ -4,3 +4,10 @@ num = 123
 for i in range(1, num+1):
   if i % 2 == 0:
     print(i, end=' ')
+    
+# is num odd or even     
+num = input("enter number")
+if num % 2 == 0:
+  print("even")
+else:
+  print("odd")
