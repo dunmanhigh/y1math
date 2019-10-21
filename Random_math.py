1d58b332b1dd4385e6fffe073ee8fc53da7f5bda
dhshoyeehsuen <38057043+dhshoyeehsuen@users.noreply.github.com>
2019-10-21 14:15:55 +0800

    Random_math
    try to solve this :D

---
+++ Random_math.py
@@ -0,0 +1,6 @@
+Yifan is playing a video game. 
+The currency of the game is known as "$"
+If Yifan passes the level, she will earn 10 $.
+However, if Yifan fails the level, she will lose 5 $.
+Every 10 minutes, Yifan is required to pay her mother $10.
+How much money does Yifan have?
