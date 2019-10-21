e5e9df310779e0c14b14d3e62ac2d22a6978c89b
dhsweeruienraeanne <38022210+dhsweeruienraeanne@users.noreply.github.com>
2019-10-21 13:48:41 +0800

    updated code for hcf of a and b

--- t01primeshcflcm_hcf2.py
+++ t01primeshcflcm_hcf2.py
@@ -3,3 +3,17 @@
 a = int(input("a: "))
 b = int(input("b: "))
 
+def hcf(x, y):  
+   if x > y:  
+       smaller = y  
+   else:  
+       smaller = x  
+   for i in range(1,smaller + 1):  
+       if((x % i == 0) and (y % i == 0)):  
+           hcf = i  
+   return hcf  
+ 
+  
+num1 = int(a)  
+num2 = int(b)  
+print("The H.C.F. of", num1,"and", num2,"is", hcf(num1, num2))  
