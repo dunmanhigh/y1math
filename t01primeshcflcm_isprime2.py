d942d8ef8363278a6138d027cbd24c9b34e23bfa
dhsjanelletan <38057030+dhsjanelletan@users.noreply.github.com>
2019-10-21 13:55:24 +0800

    Update Math Question

--- t01primeshcflcm_isprime2.py
+++ t01primeshcflcm_isprime2.py
@@ -1,12 +1,11 @@
 # Is num a prime number? 
 # A prime number is an integer with exactly 2 different factors, 1 and itself.
-# Can you rectify the following program error?
+# What happens if you add 3 to num? Does num 2 become a prime number?
 
-num = 61
+num = 28
 
+prime = False
+
+3+num= 31
+31= num 2 
 prime = True
-for i in range(2,num): # won't work for 2 which is the only even prime
-  if num % i == 0:
-    prime = False
-  
-print(prime)
