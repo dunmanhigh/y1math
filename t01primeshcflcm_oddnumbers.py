e4402ae2219eda32b78c8add6ddce254bd69d76e
dhsrauflicheng <38057063+dhsrauflicheng@users.noreply.github.com>
2019-10-21 14:35:49 +0800

    Update t01primeshcflcm_oddnumbers.py

--- t01primeshcflcm_oddnumbers.py
+++ t01primeshcflcm_oddnumbers.py
@@ -4,3 +4,10 @@ num = 123
 for i in range(1, num+1):
   if i % 2 == 1: # or != 0
     print(i, end=' ')
+
+#is the sum of 2 and 7 an odd number?
+sum = 2+7
+if sum % 2 == 1 :
+  print('yes')
+else :
+  print('no')
