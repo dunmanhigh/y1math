13596aa2aea27b4a32dd7964a28ab53ac871c8e1
dhsjanelletan <38057030+dhsjanelletan@users.noreply.github.com>
2019-10-21 14:15:35 +0800

    Changes to working

--- t01primeshcflcm_hcf1.py
+++ t01primeshcflcm_hcf1.py
@@ -1,4 +1,9 @@
 # Find the Lowest Common Multiple of 20 and 5
 # Lowest Common Multiple is the smallest number that can be divided by both numbers.
-
-Ans = 40 
+a=20
+b=5
+while a%b==0:
+  c=a/b
+  break
+ans=b*c
+print(ans)
