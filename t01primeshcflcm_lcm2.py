643a22eb919875b8bda9c73bbb0a502601c2dc4a
dhskoryutania <37990683+dhskoryutania@users.noreply.github.com>
2019-10-22 14:10:53 +0800

    Improvements

--- t01primeshcflcm_lcm2.py
+++ t01primeshcflcm_lcm2.py
@@ -1,5 +1,5 @@
-# To find LCM, we can use prime factorisation.
-# Step 1: Obtain the prime factorisation of each number
+# To find the LCM, we can use prime factorisation.
+# Step 1: Obtain the prime factor of each number
 # Step 2: Identify the common prime factors
 # Step 3: The LCM of the two numbers is the product of the common prime factors 
 #         and all the other prime factors
