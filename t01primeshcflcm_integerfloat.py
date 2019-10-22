89104b6472197723851db6f09bda6f8af7eda8de
dhslauren <38091569+dhslauren@users.noreply.github.com>
2019-10-22 14:04:54 +0800

    Update t01primeshcflcm_integerfloat.py

--- t01primeshcflcm_integerfloat.py
+++ t01primeshcflcm_integerfloat.py
@@ -9,3 +9,24 @@ print(type(num))
 num = 432222.999
 print(type(num))
 
+#include <stdio.h>
+
+void main()
+
+{
+
+float f;
+
+int i;
+
+printf("Enter a number : ");
+
+scanf("%f",&f);
+
+i=f;
+
+if(i==f)
+
+    printf("\nYou have Entered an Integer number");
+
+else
