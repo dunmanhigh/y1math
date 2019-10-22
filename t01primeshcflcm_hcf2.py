c55832af236f416846faabf16b594ccf1b1aacc5
dhstanyunqi <38091441+dhstanyunqi@users.noreply.github.com>
2019-10-22 14:24:45 +0800

    Update t01primeshcflcm_hcf2.py

--- t01primeshcflcm_hcf2.py
+++ t01primeshcflcm_hcf2.py
@@ -1,5 +1,18 @@
 # Find the Highest Common Factor (HCF) of a and b.
 
+def computeHCF(a, b):
+
+    if a > b:
+        smaller = b
+    else:
+        smaller = a
+    for i in range(1, smaller+1):
+        if((a % i == 0) and (b % i == 0)):
+            hcf = i
+            
+    return hcf
+
 a = int(input("a: "))
 b = int(input("b: "))
 
+print(computeHCF(a, b))
