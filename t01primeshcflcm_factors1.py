d67ffe8b04ece78de949ba31dd6a5c5a7dc6b229
dhslauren <38091569+dhslauren@users.noreply.github.com>
2019-10-22 14:15:07 +0800

    Update t01primeshcflcm_factors1.py

--- t01primeshcflcm_factors1.py
+++ t01primeshcflcm_factors1.py
@@ -7,3 +7,11 @@ num = 18
 for i in range(1, num+1):  
   if num % i == 0:
     print(i, end=' ')
+n = 600851475143
+i = 2
+while i * i < n:
+     while n % i == 0:
+         n = n / i
+     i = i + 1
+
+print (n)
