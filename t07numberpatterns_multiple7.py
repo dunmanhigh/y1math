c049e2725313e483e187d1d63833a89b70962fbb
dhsawzhengxi <38091474+dhsawzhengxi@users.noreply.github.com>
2019-10-22 13:46:34 +0800

    Update t07numberpatterns_multiple7.py

--- t07numberpatterns_multiple7.py
+++ t07numberpatterns_multiple7.py
@@ -1,9 +1,8 @@
 # Generate multiples of 7 from 1 to 100
 
-START = 
-END = 
-num = 
+START = 7
+END = 98
+num = +7
 
 for i in range(START, END+1):
-  if :
-    print(i, end=' ')
+    print(i, end='98 ')
