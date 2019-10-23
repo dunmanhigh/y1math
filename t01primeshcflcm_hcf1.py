d748354874606254a71c8d6b95d23d54a1ea3ad9
chickenugget123 <56907310+chickenugget123@users.noreply.github.com>
2019-10-23 13:41:48 +0800

    Update t01primeshcflcm_hcf1.py

--- t01primeshcflcm_hcf1.py
+++ t01primeshcflcm_hcf1.py
@@ -1,2 +1,4 @@
-# Find the Highest Common Factor (HCF) of 18 and 30.
-
+# Find the Highest Common Factor (HCF) of 18 and 30
+# prime factorisation of 18 = 2 x 3 x 3
+# prime factorisation of 30 = 2 x 3 x 5
+# HCF = 2 x 3 = 6
