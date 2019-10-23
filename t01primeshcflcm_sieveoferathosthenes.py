e02a1ac9dcb12281973411be199ad1bc66ab55a7
chelseayay <52947013+chelseayay@users.noreply.github.com>
2019-10-23 13:19:23 +0800

    Nathan Ong 1J

--- t01primeshcflcm_sieveoferathosthenes.py
+++ t01primeshcflcm_sieveoferathosthenes.py
@@ -1,3 +1,13 @@
 # Implement the Sieve of Erathosthenes for numbers between 1 to 100.
 
 nums = [i for i in range(1,101)]
+def prime_eratosthenes(n):
+    prime_list = []
+    for i in range(2, n+1):
+        if i not in prime_list:
+            print (i)
+            for j in range(i*i, n+1, i):
+                prime_list.append(j)
+
+print(prime_eratosthenes(100))';
+
