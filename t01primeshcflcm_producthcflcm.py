b5482e271c86ab17036d84f9c79323e61aeb359c
Jolee6808 <52901713+Jolee6808@users.noreply.github.com>
2019-10-23 13:43:59 +0800

    Update t01primeshcflcm_producthcflcm.py

--- t01primeshcflcm_producthcflcm.py
+++ t01primeshcflcm_producthcflcm.py
@@ -2,4 +2,7 @@
 
 num1 = 
 num2 = 
-
+# find the HCF and LCM of num1 and num2
+# find the product of both num1 and num2 HCF and LCM and name as num4
+# find product of both numbers as num3
+# num3 = num4
