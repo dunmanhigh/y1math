186c3487f950a709bbd115d9c920a44576426b93
EVONNESAMYANYEE <52946889+EVONNESAMYANYEE@users.noreply.github.com>
2019-10-23 13:32:56 +0800

    Evonne Sam 1j

--- t01primeshcflcm_oddnumbers.py
+++ t01primeshcflcm_oddnumbers.py
@@ -1,6 +1,12 @@
 # What are the odd numbers from 1 to num?
 
 num = 123
-for i in range(1, num+1):
-  if i % 2 == 1: # or != 0
-    print(i, end=' ')
+
+lower_limit = int(input("Enter the lower limit : 1"))
+upper_limit = int(input("Enter the upper limit : 123"))
+
+for i in range(lower_limit,upper_limit + 1):
+    #3
+    if(i%2 != 0):
+        print("{} ".format(i))
+ 
