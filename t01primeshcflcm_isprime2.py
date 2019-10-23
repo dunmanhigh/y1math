f7360ffb52a2b85943403e327fb65afe0d081472
JAMI747-400 <52901832+JAMI747-400@users.noreply.github.com>
2019-10-23 13:44:11 +0800

    Rectified the (program) problem

--- t01primeshcflcm_isprime2.py
+++ t01primeshcflcm_isprime2.py
@@ -1,12 +1,14 @@
 # Is num a prime number? 
 # A prime number is an integer with exactly 2 different factors, 1 and itself.
-# Can you rectify the following program error?
+# Can you rectify the following program error
 
 num = 61
-
-prime = True
-for i in range(2,num): # won't work for 2 which is the only even prime
+num_factors = 0
+for i in range(1,num+1):
   if num % i == 0:
-    prime = False
+    num_factors += 1
+if num_factors == 2:
+  print(num, "is prime")
+else:
+  print(num, "is not prime")
   
-print(prime)
