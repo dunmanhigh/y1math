ea2c7d0c03ccf6e0d5e4a67fcfe7befb48b8e6d5
goijinghengmatthias <52947091+goijinghengmatthias@users.noreply.github.com>
2019-10-23 13:17:42 +0800

    Update t01primeshcflcm_hcf1.py

--- t01primeshcflcm_hcf1.py
+++ t01primeshcflcm_hcf1.py
@@ -1,2 +1,26 @@
 # Find the Highest Common Factor (HCF) of 18 and 30.
+# Python program to find the H.C.F of two input number
+
+# define a function
+def computeHCF(x, y):
+
+# choose the smaller number
+    if x > y:
+        smaller = y
+    else:
+        smaller = x
+    for i in range(1, smaller+1):
+        if((x % i == 0) and (y % i == 0)):
+            hcf = i
+            
+    return hcf
+
+num1 = 18 
+num2 = 30
+
+# take input from the user
+# num1 = int(input("Enter first number: "))
+# num2 = int(input("Enter second number: "))
+
+print("The H.C.F. of", num1,"and", num2,"is", computeHCF(num1, num2))
 
