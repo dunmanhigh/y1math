6ae15b854b7f60dfe12c92066e478a1756717e9d
TallerThanHR <52901752+TallerThanHR@users.noreply.github.com>
2019-10-23 13:42:11 +0800

    Update t01primeshcflcm_isprime3.py

--- t01primeshcflcm_isprime3.py
+++ t01primeshcflcm_isprime3.py
@@ -3,4 +3,11 @@
 # Consider implementing stop checking at int(num ** 0.5) for efficiency.
 
 num = 61
-
+num_factors = 0
+for i in range(1,num+1):
+  if num % i == 0:
+    num_factors += 1
+if num_factors == 2:
+  print(num, "is prime")
+else:
+  print(num, "is not prime")
