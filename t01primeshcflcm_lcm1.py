5c06615939c6a4a3fedc042e5bb4fe6c794e6dc4
EVONNESAMYANYEE <52946889+EVONNESAMYANYEE@users.noreply.github.com>
2019-10-23 13:26:37 +0800

    Evonne Sam 1J

--- t01primeshcflcm_lcm1.py
+++ t01primeshcflcm_lcm1.py
@@ -1,2 +1,23 @@
 # Find the Lowest Common Multiple (LCM) of 18 and 30.
 
+
+def gcd(x, y):
+  
+
+   while(y):
+       x, y = y, x % y
+
+   return x
+
+
+def lcm(x, y):
+  
+   lcm = (x*y)//gcd(x,y)
+   return lcm
+
+
+num1 = 30
+num2 = 18 
+
+
+print("The L.C.M. of", num1,"and", num2,"is", lcm(num1, num2))
