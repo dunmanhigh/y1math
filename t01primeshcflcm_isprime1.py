5c4bbdf7e2c9ec54b2b6c06b61441068c03ba258
zhitinggg <52991684+zhitinggg@users.noreply.github.com>
2019-10-24 14:30:59 +0800

    tan zhi ting

--- t01primeshcflcm_isprime1.py
+++ t01primeshcflcm_isprime1.py
@@ -7,7 +7,7 @@ for i in range(1,num+1):
   if num % i == 0:
     num_factors += 1
 if num_factors == 2:
-  print(num, "is prime")
+  print(num, "is prime")    
 else:
   print(num, "is not prime")
   
