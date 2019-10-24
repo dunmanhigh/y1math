80f38f350a5342013284ad392554dba46aace924
yiwei1H <53069861+yiwei1H@users.noreply.github.com>
2019-10-24 14:42:27 +0800

    Update t01primeshcflcm_evennumbers.py

--- t01primeshcflcm_evennumbers.py
+++ t01primeshcflcm_evennumbers.py
@@ -1,6 +1,6 @@
 # What are the even numbers from 1 to num?
 
 num = 123
-for i in range(1, num+1):
+for i in range(1, num+1): # i will iterate through 1 to 124
   if i % 2 == 0:
     print(i, end=' ')
