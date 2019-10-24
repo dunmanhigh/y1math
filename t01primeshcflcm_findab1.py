97414aba6ae8cee26810a6632682f0d67377365b
zhitinggg <52991684+zhitinggg@users.noreply.github.com>
2019-10-24 14:20:28 +0800

    tan zhi ting (1G)

--- t01primeshcflcm_findab1.py
+++ t01primeshcflcm_findab1.py
@@ -1,2 +1,17 @@
 # If a and b are integers such that a * b = 17, find the value of a + b.
 
+# a = 1 so b= a -1
+# a = 2 so b = a -2
+# if a is < b, continue. 
+
+c = 0
+a = 1
+b = 18/a
+
+while a<=b:
+  c = a+b
+  if b == int(b):
+    #print(str(a)+":" +str(b)+":" +str(c), end=' ')
+    print(str(c), end=' ')
+  a = a+1
+  b= 18/a
