fb1dac5354559773bf256d828f81ce45440fc1d1
meguurnn <52988134+meguurnn@users.noreply.github.com>
2019-10-24 15:13:47 +0800

    answered question no.3

--- t07numberpatterns_multiple7.py
+++ t07numberpatterns_multiple7.py
@@ -1,9 +1,7 @@
 # Generate multiples of 7 from 1 to 100
 
-START = 
-END = 
-num = 
+START = 7
+END = 98
+num = 14 numbers
 
-for i in range(START, END+1):
-  if :
-    print(i, end=' ')
+the multiples are = 7,14,21,28,35,42,49,56,63,70,77,384,91,98
