f3cf1d5eb669b2faaff61c0fc2d2ac134b12c154
wongweizhi1e <53029646+wongweizhi1e@users.noreply.github.com>
2019-10-24 14:21:29 +0800

    Update t01primeshcflcm_primefactorisation.py

--- t01primeshcflcm_primefactorisation.py
+++ t01primeshcflcm_primefactorisation.py
@@ -1,5 +1,5 @@
 # The process of expressing a number as a product of its prime factors is called prime factorisation.
 # Express 18 as a prime factorisation.
 
-prime factorisation = ("2","3","3")
+prime factorisation = ("2x","3x","3")
 print("18" = prime factorisation)
