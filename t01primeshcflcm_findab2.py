46c2b714be90b15271b47737d5f177123e05da4e
JAMI747 <56918758+JAMI747@users.noreply.github.com>
2019-10-31 13:34:11 +0800

    Added in the answers of the possible values

--- t01primeshcflcm_findab2.py
+++ t01primeshcflcm_findab2.py
@@ -1,2 +1,9 @@
 # If a and b are integers such that a * b = 18, find the possible values of a + b.
 
+# a * b = 18
+# 3 * 6 = 18
+# 2 * 9 = 18
+# 1 * 18 = 18
+# Possible values of a + b = 3 + 6 = 9
+# Possible values of a + b = 2 + 9 = 11
+# Possible values of a + b = 1 + 18 = 19 
