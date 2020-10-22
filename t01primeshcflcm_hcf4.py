c77a2b823033be4b5d32b52cdb2541f6f17f7683
asdiadenpotatisen <69951589+asdiadenpotatisen@users.noreply.github.com>
2020-10-22 13:52:30 +0800

    Update t01primeshcflcm_hcf4.py

--- t01primeshcflcm_hcf4.py
+++ t01primeshcflcm_hcf4.py
@@ -1,4 +1,14 @@
 # Find the HCF of n positive integers.
 
-n = 
-
+def gcd(a, b): 
+    while(b): 
+        (a, b) = (b, a % b)
+    return a
+  
+numbers = [1, 2, 3, 4, 5] 
+num1 = numbers[0] 
+num2 = numbers[1]
+gcdnum = gcd(num1,num2)
+for i in range(2,len(numbers)): 
+    gcdnum  = gcd(gcdnum, numbers[i]) 
+print(gcd) 
