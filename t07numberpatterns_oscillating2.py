399209886520cc359e03964276f5abc3c748a348
asdiadenpotatisen <69951589+asdiadenpotatisen@users.noreply.github.com>
2020-10-22 14:01:05 +0800

    Update t07numberpatterns_oscillating2.py

--- t07numberpatterns_oscillating2.py
+++ t07numberpatterns_oscillating2.py
@@ -1,6 +1,12 @@
 # Generate oscillating pattern 1, -1, 2, -2, 3, -3, ... k times
 
 k = 10
+num = 1
 
 for i in range(1, k+1):
-  
+  if i % 2 == 1:
+    num = num-(i+1)
+    print(num)
+  if i % 2 == 0:
+    num = num+(i+1)
+    print(num)
